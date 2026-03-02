aea0962f42eedc5e8e5b73594e0e7be4cd700155
Skipper Lizbeth Thurman <slthurman01@gmail.com>
2026-03-01 21:16:36 -0600

    code for translating audio data into manageable numbers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import wave
+import numpy as np
 
 # Open the WAV file
 with wave.open("glow.wav", "r") as f:
@@ -12,8 +13,29 @@ with wave.open("glow.wav", "r") as f:
     # Calculate duration
     duration = num_frames / sample_rate
 
+    # Read raw bytes from the file
+    raw_bytes = f.readframes(f.getnframes())
+
+# Convert bytes into numbers
+audio_data = np.frombuffer(raw_bytes, dtype=np.int16)
+
+
+
+print(f"Type: {type(audio_data)}")
+print (f"Shape: {audio_data.shape}")
+print(f"First 20 samples: {audio_data[:20]}")
+print(f"Min value: {audio_data.min()}")
+print(f"Max value: {audio_data.max()}")
+print(f"Data type: {audio_data.dtype}")
+
+
+
+
+"""
     print (f"Sample rate: {sample_rate} Hz")
     print(f"Channels: {num_channels}")
     print(f"Bit depth: {bit_depth} bit")
     print(f"Number of samples: {num_frames}")
     print(f"Duration: {duration:.2f} seconds")
+"""
+
